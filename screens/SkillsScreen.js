4bf1d8fcc58cb856136734d0e40c9fe8244ecd06
Bayon Forte <bayon@forteworks.com>
2021-02-01 14:29:47 -0500

    doctor the card to accept params for examples and llinks.

--- screens/SkillsScreen.js
+++ screens/SkillsScreen.js
@@ -3,9 +3,10 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 //const jwtDecode = require("jwt-decode");
 import jwt_decode from "jwt-decode";
 import React, { useEffect, useState } from "react";
-import { Button, Linking, Platform, Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
 import { useSelector } from "react-redux";
 import FormStyles from "./FormStyles";
+import Card from "./ui_components/Card";
 
 const styles = FormStyles;
 
@@ -43,72 +44,72 @@ const SkillsScreen = (props) => {
   }
 
   return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.text}>
-          Skills Screen: Welcome {fullName ? fullName : ""}
-        </Text>
-        <Text>Skills</Text>
-        <Text
-          style={{ color: "blue" }}
-          onPress={() =>
-            {
-              const url = "http://www.forteworks.com/forteworks-16.9.19/games/index-games.php";
-              if(Platform.OS == 'web'){
-                window.open(url, '_blank');
-             } else {
-              Linking.openURL(url,'_blank')
 
-             }
-            }
-           
-          }
-        >
-          Games
-        </Text>
-
-        <Text
-          style={{ color: "blue" }}
-          onPress={() =>
-            {
-              const url = "http://www.forteworks.com/roughest/";
-              if(Platform.OS == 'web'){
-                window.open(url, '_blank');
-             } else {
-              Linking.openURL(url,'_blank')
-
-             }
-            }
-           
-          }
-        >
-          Estimation App
-        </Text>
+    <Card 
+    image="http://www.forteworks.com/forteworks-16.9.19/games/images/spaceshooter.jpg"
+    title="Games"
+    description="A set of games created using some library, I'll have to look it up, customized to fit branding of customer.Also modified for gesture recognition."
+    linkName="Estimation App"
+    linkURL="http://www.forteworks.com/roughest/"
+    
+    ></Card>
 
 
-        <Text
-          style={{ color: "blue" }}
-          onPress={() =>
-            {
-              const url = "https://drive.google.com/file/d/0B6WIP-y6MAiEYm11b1hQN1Z0Sms/view";
-              if(Platform.OS == 'web'){
-                window.open(url, '_blank');
-             } else {
-              Linking.openURL(url,'_blank')
+    // <View style={styles.container}>
+    //   <Text style={styles.text}>
+    //     Skills Screen: Welcome {fullName ? fullName : ""}
+    //   </Text>
+    //   <Text>Skills</Text>
+    //   <Text
+    //     style={{ color: "blue" }}
+    //     onPress={() => {
+    //       const url =
+    //         "http://www.forteworks.com/forteworks-16.9.19/games/index-games.php";
+    //       if (Platform.OS == "web") {
+    //         window.open(url, "_blank");
+    //       } else {
+    //         Linking.openURL(url, "_blank");
+    //       }
+    //     }}
+    //   >
+    //     Games
+    //   </Text>
 
-             }
-            }
-           
-          }
-        >
-          Screencast for Estimation App
-        </Text>
- 
-      </View>
-      <View>
-        <Text style={styles.text}>Your email is {email ? email : ""}</Text>
-      </View>
-    </View>
+    //   <Text
+    //     style={{ color: "blue" }}
+    //     onPress={() => {
+    //       const url = "http://www.forteworks.com/roughest/";
+    //       if (Platform.OS == "web") {
+    //         window.open(url, "_blank");
+    //       } else {
+    //         Linking.openURL(url, "_blank");
+    //       }
+    //     }}
+    //   >
+    //     Estimation App
+    //   </Text>
+
+    //   <Text
+    //     style={{ color: "blue" }}
+    //     onPress={() => {
+    //       const url =
+    //         "https://drive.google.com/file/d/0B6WIP-y6MAiEYm11b1hQN1Z0Sms/view";
+    //       if (Platform.OS == "web") {
+    //         window.open(url, "_blank");
+    //       } else {
+    //         Linking.openURL(url, "_blank");
+    //       }
+    //     }}
+    //   >
+    //     Screencast for Estimation App
+    //   </Text>
+    //   <View>
+    //     <Text style={styles.text}>Your email is {email ? email : ""}</Text>
+    //   </View>
+     
+    // </View>
+
+    //http://www.forteworks.com/forteworks-16.9.19/games/images/spaceshooter.jpg
   );
 };
 
